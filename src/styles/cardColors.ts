7a429902520ed787159a8a2e9ee19c9ea9c8a543
Samuga <47028066+samuelgarciacaballero@users.noreply.github.com>
2025-08-13 04:34:27 +0200

    chore: remove card-back binary asset

--- src/styles/cardColors.ts
+++ src/styles/cardColors.ts
@@ -1,7 +1,7 @@
-export const rarityColors: Record<string,string> = {
-    normal:    '#d3d3d3',  // gris claro
-    rare:      '#a8e6cf',  // verde claro
-    epic:      '#d291e4',  // morado claro
-    // mythic:    '#ffb347',  // naranja
-    legendary: '#ffd700',  // dorado
-  };
+export const rarityColors: Record<string, string> = {
+  normal: '#d3d3d3',      // gris claro
+  rare: '#a8e6cf',        // verde claro
+  epic: '#d291e4',        // morado claro
+  mythic: '#ffb347',      // naranja
+  legendary: '#ffd700',   // dorado
+};
